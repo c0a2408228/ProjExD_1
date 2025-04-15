1402045a407fe938b49c274e8cdb4e9d631e5098
c0a2408228 <c0a2408228@edu.teu.ac.jp>
2025-04-15 16:54:54 +0900

    課題2

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -21,6 +21,8 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x1 = -1
+        y1 = 0
         key_lst = pg.key.get_pressed()
 
         screen.blit(bg_img, [-x, 0])
@@ -34,15 +36,15 @@ def main():
         if x == 3200:
             x = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            y1 = -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-2,0))
-        else:
-            kk_rct.move_ip((-1,0))
+            y1 = 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0))
+            x1 = 2
+        if key_lst[pg.K_LEFT]:
+            x1 = -2
+        
+        kk_rct.move_ip((x1,y1))
         
         
 
