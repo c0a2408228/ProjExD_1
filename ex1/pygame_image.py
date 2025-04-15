2ad05d8d30fbc55d400b335e22f52d5f9590ef02
c0a2408228 <c0a2408228@edu.teu.ac.jp>
2025-04-15 17:11:28 +0900

    演習課題3

---
+++ ex1/pygame_image.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
+    kk_rct = kk_img.get_rect() #練習10-1
+    kk_rct.center = 300, 200 #練習10-2
+    tmr = 0
+    x = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        x1 = -1
+        y1 = 0
+        key_lst = pg.key.get_pressed()
+
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(kk_img,kk_rct) #練習10-5
+        pg.display.update()
+        tmr += 1       
+        x += 1 
+        clock.tick(200)
+        if x == 3200:
+            x = 0
+        if key_lst[pg.K_UP]:
+            y1 = -1
+        if key_lst[pg.K_DOWN]:
+            y1 = 1
+        if key_lst[pg.K_RIGHT]:
+            x1 = 2
+        if key_lst[pg.K_LEFT]:
+            x1 = -2
+        
+        kk_rct.move_ip((x1,y1))
+        
+        
+
+            
+
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
