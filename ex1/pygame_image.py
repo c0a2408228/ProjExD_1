0ff5e97b0155ba6c8f3ee8e8d7c19fe1f09dbab5
c0a2408228 <c0a2408228@edu.teu.ac.jp>
2025-04-15 15:56:26 +0900

    練習問題７

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -18,12 +18,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [-x+1600, 0])
         screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1       
-        x -= 1 
+        x += 1 
         clock.tick(200)
+
+            
+
 
 
 if __name__ == "__main__":
